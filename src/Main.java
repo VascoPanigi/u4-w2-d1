3915f385e0616106dedb8e9109bd07a88e3622ad
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-05-27 15:57:05 +0200

    adds ex - 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Insert a string and I'll count the number of 'A' in it. ");
+//
+//        String sentence = scanner.nextLine();
+//
+//        for (int i = 0; i < sentence.length(); i++) {
+//            char c = sentence.charAt(i);
+//            if (c == 'a') {
+//                System.out.println(c);
+//            }
+//        }
+//
+//        scanner.close();
+
+        int[] newArray = new int[5];
+
+        Random random = new Random();
+
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = random.nextInt(1, 10);
+        }
+
+        System.out.println(Arrays.toString(newArray));
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Choose a number. Type 0 to get the * out.");
+            int newNumber = Integer.parseInt(scanner.nextLine());
+            System.out.println("Choose its position.");
+            int numberPosition = Integer.parseInt(scanner.nextLine());
+            System.out.println("daje");
+
+            
+            if (newNumber != 0) {
+                newArray[numberPosition] = newNumber;
+                System.out.println("There you go, happy now?");
+                logger.info("Happy gabibbo");
+                System.out.println(Arrays.toString(newArray));
+            } else {
+                logger.info("You got the * out");
+                break;
+            }
+
+
+        }
+
+
     }
 }
